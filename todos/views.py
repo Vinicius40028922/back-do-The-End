e76b0853812344705b36dc8e2d03635066af27d4
Cleitinho <email@falso.com>
2024-09-03 21:51:46 -0300

    model

--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Todo
+from django.views.generic import ListView
+    
+def todo_list_old(request):
+    nome = 'Rodrigo'
+    alunos = ['1. Ana', '2. José', '3. Bia']
+    return render(request, "todos/todo_list.html", {"nome": nome, "lista_alunos": alunos})
 
-def home(request):
-    return HttpResponse('Mundo da Ola')
+def todo_list(request):
+    todos = Todo.objects.all()
+    return render(request, "todos/todo_list.html", {"todos": todos})
+
+
+class TodoListView(ListView):
+    model = Todo
